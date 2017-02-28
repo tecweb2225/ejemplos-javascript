78fa9b336cb24a295217a964cbbf1ef365ec6521
Eloy Hernández <eloyhz@gmail.com>
2017-02-28 13:00:10 -0600

    Primer ejemplo DOM

---
+++ dom1/main.js
@@ -0,0 +1,5 @@
+//var cajas = document.getElementsByTagName('div')
+var cajas = document.getElementsByClassName('caja')
+
+cajas[0].textContent = 'Hola Mundo'
+cajas[1].innerHTML = '<b>Hola JavaScript</b>'
